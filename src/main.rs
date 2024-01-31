0d17a7a7ae1b9e5fc9f3dd629b391260c520812f
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-02-01 03:21:22 +0900

    first impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{stdin, stdout, Write};
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+
+        let mut input = String::from("");
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .unwrap();
+
+        child.wait().unwrap();
+    }
 }
